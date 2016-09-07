5b43a6aa5439417bbc2fe9fc6919c78e0ecb3746
Carlos Guedes <cguedes@gmail.com>
2016-09-07 09:12:50 +0100

    Initial version of node-sheets lib

---
+++ README.md
@@ -0,0 +1,70 @@
+# node-sheets
+
+Read rows from google spreadsheet with google's sheets api.
+
+> This library is beta so you should use it with care. We will make an effort to support the library, but we reserve the right to make incompatible changes when necessary.
+
+## Instalation
+
+This library is distributed on `npm`. In order to add it as a dependency, run the following command:
+
+
+```bash
+$ npm install node-sheets --save
+```
+
+## Usage
+
+Example to retrieve data from [this google spreadsheet](https://docs.google.com/spreadsheets/d/1amfst1WVcQDntGe6walYt-4O5SCrHBD5WntbjhvfIm4) using ES7 async/await.
+
+```javascript
+try {
+  const gs = new Sheets('1amfst1WVcQDntGe6walYt-4O5SCrHBD5WntbjhvfIm4')
+  const authData = require('someGoogleCredentials.json') // authData = { client_email, private_key }
+  await gs.authorizeJWT(authData)
+  const table = gs.table('Formats!A1:E3')
+  console.log(table.headers)
+  console.log(table.formats)
+  console.log(table.rows)
+} catch(err) {
+  console.error(err)
+}
+```
+
+You can also use the lib with Promises.
+
+```javascript
+const gs = new Sheets('1amfst1WVcQDntGe6walYt-4O5SCrHBD5WntbjhvfIm4')
+const authData = require('someGoogleCredentials.json') // authData = { client_email, private_key }
+gs.authorizeJWT(authData)
+  .then(() => gs.table('Formats!A1:E3'))
+  .then(table => {
+    console.log(table.headers)
+    console.log(table.formats)
+    console.log(table.rows)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+```
+
+## Authentication
+
+For now, node-sheets offers two authentication mechanisms.
+
+ 1. With JWT token (`.authorizeJWT(auth [, scopes])`) using `private_key` and `client_email`, and also allowing to set auth scopes. The default auth scope is https://www.googleapis.com/auth/spreadsheets.readonly.
+
+ 1. With APIKEY (`.authorizeApiKey(apikey)`) using an API Key you have created in the [google developers console](https://console.developers.google.com).
+
+## Examples
+
+You can check the `/test/index.js` file for examples.
+
+
+## License
+
+This library is licensed under MIT. Full license text is available in [LICENSE](LICENSE).
+
+## Contributing
+
+We appreciate any contribution to this project. We keep a list of features and bugs in the [issue tracker](https://github.com/urbancups/node-sheets/issue).
---
+++ test/index.js
@@ -0,0 +1,130 @@
+import assert from 'assert'
+import Sheets from './../src'
+import util from 'util'
+
+const SPREADSHEET_TEST_ID = '1amfst1WVcQDntGe6walYt-4O5SCrHBD5WntbjhvfIm4'
+const SPREADSHEET_JWT_KEY = require('./../cred/node-sheets-test-e9b53a714340.json')
+const SPREADSHEET_API_KEY = 'AIzaSyAwwdh_6ktghYF_AgP5pT9EfeiYWVCTr1Q'
+
+describe('Sheets', function () {
+  this.timeout(10000)
+
+  describe('#constructor', function () {
+    it('should exist and be a function', () => {
+      assert.equal(typeof Sheets, 'function')
+    })
+
+    it('should throw Error with empty spreadsheetId in the constructor', () => {
+      assert.throws(() => { new Sheets() }, Error)
+    })
+
+    it('should accept a string with spreadsheetId', () => {
+      assert.notEqual(new Sheets(SPREADSHEET_TEST_ID), null)
+    })
+  })
+
+  describe('#authorize', function () {
+    const gs = new Sheets(SPREADSHEET_TEST_ID)
+
+    it('should throw Error with no args in authorizeJWT', async () => {
+      try {
+        await gs.authorizeJWT()
+        assert.fail()
+      } catch(err) {
+        assert.equal(err.constructor, Error)
+      }
+    })
+
+    it('should throw Error with no args in authorizeApiKey', async () => {
+      try {
+        await gs.authorizeJWT()
+        assert.fail()
+      } catch(err) {
+        assert.equal(err.constructor, Error)
+      }
+    })
+
+    it('should authenticate with valid JWT credentials', async () => {
+      await gs.authorizeJWT(SPREADSHEET_JWT_KEY)
+    })
+
+    it('should authenticate with valid API KEY credentials', async () => {
+      await gs.authorizeApiKey(SPREADSHEET_API_KEY)
+    })
+
+  })
+
+  describe('#table (Formats!A1:E3)', () => {
+    const gs = new Sheets(SPREADSHEET_TEST_ID)
+    before(async () => {
+      await gs.authorizeApiKey(SPREADSHEET_API_KEY)
+    })
+
+    it('should return formatted tabular spreadsheet data', async () => {
+      const table = await gs.table('Formats!A1:E3')
+      // console.log(util.inspect(table, { depth: null, colors: true }))
+      assert.deepEqual(table.headers, [ 'Automatic', 'Currency', 'Date', 'Number', 'Plain Text' ])
+      assert.deepEqual(table.formats.map(f => f.numberFormat.type), [ 'NONE', 'CURRENCY', 'DATE', 'NUMBER', 'TEXT' ])
+      assert.equal(table.rows.length, 2)
+      assert.deepEqual(table.rows[0].stringValues, [ 'Oil', '$0.41', '1/25/2016', '123.00', 'This is some text' ])
+      assert.deepEqual(table.rows[0].values, [ 'Oil', .41, new Date(2016, 0, 25), 123, 'This is some text' ])
+    })
+
+    it('should return formatted tabular (tableCols) spreadsheet data', async () => {
+      const cols = await gs.tableCols('Formats!A1:E3')
+      assert.equal(cols.length, 5)
+      assert.deepEqual(cols.map(c => c.header), [ 'Automatic', 'Currency', 'Date', 'Number', 'Plain Text' ])
+      assert.equal(cols[0].header, 'Automatic')
+      assert.deepEqual(cols[0].values, ['Oil', 'Grass'])
+    })
+  })
+
+  describe('#table (Class Data)', () => {
+    const gs = new Sheets(SPREADSHEET_TEST_ID)
+    before(async () => {
+      await gs.authorizeApiKey(SPREADSHEET_API_KEY)
+    })
+
+    it('should retrieve only cols and rows with content (8 cols and 2 rows - from "Class Data")', async () => {
+      const table = await gs.table('Class Data')
+      assert.equal(table.headers.length, 8)
+      assert.equal(table.rows.length, 2)
+      // console.log(util.inspect(table, { depth: null, colors: true, breakLength: Infinity }))
+    })
+
+    it('should retrieve empty cells as undefined (5 cols and 3 rows - from "Table with empty cells")', async () => {
+      const table = await gs.table('Table with empty cells')
+      // console.log(util.inspect(table, { depth: null, colors: true, breakLength: Infinity }))
+      assert.equal(table.headers.length, 5)
+      assert.equal(table.rows.length, 3)
+      assert.deepEqual(table.rows[1].values, ['Andrew', undefined, '1. Freshman', 'SD', 'Math'])
+      assert.deepEqual(table.rows[2].values, ['Anna', 'Female', undefined, undefined, 'English'])
+    })
+
+  })
+
+  describe('works with Promises', () => {
+    it('should throw Error with no args in authorization', () => {
+      const gs = new Sheets(SPREADSHEET_TEST_ID)
+      gs.authorizeJWT()
+        .then(() => assert.fail())
+        .catch(err => assert.equal(err.constructor, Error))
+    })
+
+    it('should be able to chain .then() calls, and also .catch()', () => {
+      const gs = new Sheets(SPREADSHEET_TEST_ID)
+      const authData = SPREADSHEET_JWT_KEY
+      gs.authorizeJWT(authData)
+        .then(() => gs.table('Formats!A1:E3'))
+        .then(table => {
+          assert.notEqual(table.headers, null)
+          assert.notEqual(table.formats, null)
+          assert.notEqual(table.rows, null)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    })
+  })
+
+})
